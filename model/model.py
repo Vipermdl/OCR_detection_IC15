d54439fd35fbe7e4df00b0b8c6183e78b87bd012
luotao <luotao@renrenche.com>
2019-04-22 19:15:42 +0800

    实现完整版FOTS模型

--- model/model.py
+++ model/model.py
@@ -1,43 +1,182 @@
-from base import BaseModel
 import torch.nn as nn
-import torch.nn.functional as F
+import torch
+import math
+
+from base import BaseModel
+from utils.bbox import Toolbox
 from .modules import shared_conv
-from .modules.pspnet import PSPNet
+from .modules.roi_rotate import ROIRotate
+from .modules.crnn import CRNN
 import pretrainedmodels as pm
+import torch.optim as optim
+import numpy as np
 
 
-class FOTSModel(BaseModel):
+class FOTSModel:
 
-    def __init__(self, config):
-        super().__init__(config)
-        self.mode = config['mode']
+    def __init__(self, config, keys='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.'):
+        self.mode = config['model']['mode']
 
         bbNet =  pm.__dict__['resnet50'](pretrained='imagenet') # resnet50 in paper
-        self.sharedConv = shared_conv.SharedConv(bbNet)
-        #self.sharedConv = PSPNet()
-        self.recognizer = Recognizer()
+        self.sharedConv = shared_conv.SharedConv(bbNet, config)
 
-    def forward(self, input):
+        nclass = len(keys) + 1
+        self.recognizer = Recognizer(nclass, config)
+        self.detector = Detector(config)
+        self.roirotate = ROIRotate()
+
+        def backward_hook(self, grad_input, grad_output):
+            for g in grad_input:
+                g[g != g] = 0  # replace all nan/inf in gradients to zero
+
+        self.recognizer.register_backward_hook(backward_hook)
+        self.detector.register_backward_hook(backward_hook)
+
+    def parallelize(self):
+        self.sharedConv = torch.nn.DataParallel(self.sharedConv)
+        self.recognizer = torch.nn.DataParallel(self.recognizer)
+        self.detector = torch.nn.DataParallel(self.detector)
+        #self.roirotate = torch.nn.DataParallel(self.roirotate)
+
+    def to(self, device):
+        self.sharedConv = self.sharedConv.to(device)
+        self.detector = self.detector.to(device)
+        self.recognizer = self.recognizer.to(device)
+
+    def summary(self):
+        self.sharedConv.summary()
+        self.detector.summary()
+        self.recognizer.summary()
+
+    def optimize(self, optimizer_type, params):
+        optimizer = getattr(optim, optimizer_type)(
+            [
+                {'params': self.sharedConv.parameters()},
+                {'params': self.detector.parameters()},
+                {'params': self.recognizer.parameters()},
+            ],
+            **params
+        )
+        return optimizer
+
+    def train(self):
+        self.sharedConv.train()
+        self.detector.train()
+        self.recognizer.train()
+
+    def eval(self):
+        self.sharedConv.eval()
+        self.detector.eval()
+        self.recognizer.eval()
+
+    def state_dict(self):
+        sd = {
+            '0': self.sharedConv.state_dict(),
+            '1': self.detector.state_dict(),
+            '2': self.recognizer.state_dict()
+        }
+        return sd
+
+    def load_state_dict(self, sd):
+        self.sharedConv.load_state_dict(sd['0'])
+        self.detector.load_state_dict(sd['1'])
+        self.recognizer.load_state_dict(sd['2'])
+
+    @property
+    def training(self):
+        return self.sharedConv.training and self.detector.training and self.recognizer.training
+
+    def parameters(self):
+        for m_module in [self.sharedConv,self.recognizer,self.detector]:
+            for m_para in m_module.parameters():
+                yield m_para
+
+    def forward(self, *input):
         '''
 
         :param input:
         :return:
         '''
+        image, boxes, mapping = input
 
-        score_map, geo_map = self.sharedConv.forward(input)
+        if image.is_cuda:
+            device = image.get_device()
+        else:
+            device = torch.device('cpu')
+        feature_map = self.sharedConv.forward(image)
 
-        if self.mode == 'detection':
-            return score_map, geo_map, None
-        elif self.mode == 'recognition':
-            recog_map = self.recognizer(score_map, geo_map)
-            return score_map, geo_map, recog_map
+        score_map, geo_map = self.detector(feature_map)
+
+        if self.training:
+            rois, lengths, indices = self.roirotate(feature_map, boxes[:, :8], mapping)
+            pred_mapping = mapping
+            pred_boxes = boxes
+        else:
+            score = score_map.permute(0, 2, 3, 1)
+            geometry = geo_map.permute(0, 2, 3, 1)
+            score = score.detach().cpu().numpy()
+            geometry = geometry.detach().cpu().numpy()
+
+            timer = {'net': 0, 'restore': 0, 'nms': 0}
+
+            pred_boxes = []
+            pred_mapping = []
+            for i in range(score.shape[0]):
+                cur_score = score[i, :, :, 0]
+                cur_geometry = geometry[i, :, :, ]
+                detected_boxes, _ = Toolbox.detect(score_map=cur_score, geo_map=cur_geometry, timer=timer)
+                if detected_boxes is None:
+                    continue
+                num_detected_boxes = detected_boxes.shape[0]
+
+                if len(detected_boxes) > 0:
+                    pred_mapping.append(np.array([i] * num_detected_boxes))
+                    pred_boxes.append(detected_boxes)
+
+            if len(pred_mapping) > 0:
+                pred_boxes = np.concatenate(pred_boxes)
+                pred_mapping = np.concatenate(pred_mapping)
+                rois, lengths, indices = self.roirotate(feature_map, pred_boxes[:, :8], pred_mapping)
+            else:
+                return score_map, geo_map, (None, None), pred_boxes, pred_mapping, None
+
+        lengths = torch.tensor(lengths).to(device)
+        preds = self.recognizer(rois, lengths)
+        preds = preds.permute(1, 0, 2) # B, T, C -> T, B, C
+
+        return score_map, geo_map, (preds, lengths), pred_boxes, pred_mapping, indices
 
 
-class Recognizer(nn.Module):
+class Recognizer(BaseModel):
 
-    def __init__(self):
-        super(Recognizer, self).__init__()
+    def __init__(self, nclass, config):
+        super().__init__(config)
+        self.crnn = CRNN(8, 32, nclass, 256)
+
+    def forward(self, rois, lengths):
+        return self.crnn(rois, lengths)
 
 
-    def forward(self, *input):
-        return None
+class Detector(BaseModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.scoreMap = nn.Conv2d(32, 1, kernel_size = 1)
+        self.geoMap = nn.Conv2d(32, 4, kernel_size = 1)
+        self.angleMap = nn.Conv2d(32, 1, kernel_size = 1)
+
+    def forward(self, feature_map):
+        final = feature_map
+        score = self.scoreMap(final)
+        score = torch.sigmoid(score)
+
+        geoMap = self.geoMap(final)
+        # 出来的是 normalise 到 0 -1 的值是到上下左右的距离，但是图像他都缩放到  512 * 512 了，但是 gt 里是算的绝对数值来的
+        geoMap = torch.sigmoid(geoMap) * 512
+
+        angleMap = self.angleMap(final)
+        angleMap = (torch.sigmoid(angleMap) - 0.5) * math.pi / 2
+
+        geometry = torch.cat([geoMap, angleMap], dim=1)
+
+        return score, geometry
