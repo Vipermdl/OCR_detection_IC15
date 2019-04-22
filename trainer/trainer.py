59830df36af05bc4410aca34554bf16ad3160298
luotao <luotao@renrenche.com>
2019-04-22 19:14:30 +0800

    实现在完整FOTS的模型情况下的训练代码打通

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch
+
 from base import BaseTrainer
 from utils.bbox import Toolbox
-# from utils.visualize import Visualizer
+from utils.util import strLabelConverter
+
+
 class Trainer(BaseTrainer):
     """
     Trainer class
@@ -11,9 +14,12 @@ class Trainer(BaseTrainer):
         Inherited from BaseTrainer.
         self.optimizer is by default handled by BaseTrainer based on config.
     """
-    def __init__(self, model, loss, metrics, resume,finetune, config,
-                 data_loader, toolbox: Toolbox, valid_data_loader=None, train_logger=None):
-        super(Trainer, self).__init__(model, loss, metrics, resume, finetune,config, train_logger)
+
+    def __init__(self, model, loss, metrics,
+                 finetune, resume, config,
+                 data_loader, toolbox: Toolbox, valid_data_loader=None, train_logger=None,
+                 keys='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.'):
+        super(Trainer, self).__init__(model, loss, metrics, finetune, resume, config, train_logger)
         self.config = config
         self.batch_size = data_loader.batch_size
         self.data_loader = data_loader
@@ -21,7 +27,7 @@ class Trainer(BaseTrainer):
         self.valid = True if self.valid_data_loader is not None else False
         self.log_step = int(np.sqrt(self.batch_size))
         self.toolbox = toolbox
-        # self.visdom = Visualizer(env='FOTS')
+        self.labelConverter = strLabelConverter(keys)
 
     def _to_tensor(self, *tensors):
         t = []
@@ -29,14 +35,9 @@ class Trainer(BaseTrainer):
             t.append(__tensors.to(self.device))
         return t
 
-    def _eval_metrics(self, output, target, mask):
-        acc_metrics = np.zeros(len(self.metrics))
-        output = output.cpu().data.numpy()
-        target = target.cpu().data.numpy()
-        output = np.argmax(output, axis=1)
-        for i, metric in enumerate(self.metrics):
-            acc_metrics[i] += metric(output, target)
-        return acc_metrics
+    def _eval_metrics(self, pred, gt):
+        precious, recall, hmean = self.metrics[0](pred, gt)
+        return np.array([precious, recall, hmean])
 
     def _train_epoch(self, epoch):
         """
@@ -57,35 +58,69 @@ class Trainer(BaseTrainer):
         self.model.train()
 
         total_loss = 0
-        total_metrics = np.zeros(len(self.metrics))
+        total_metrics = np.zeros(3)  # precious, recall, hmean
         for batch_idx, gt in enumerate(self.data_loader):
-            img, score_map, geo_map, training_mask, transcript = gt
-            img, score_map, geo_map, training_mask = self._to_tensor(img, score_map, geo_map, training_mask)
-            recog_map = None
+            try:
+                imagePaths, img, score_map, geo_map, training_mask, transcripts, boxes, mapping = gt
+                img, score_map, geo_map, training_mask = self._to_tensor(img, score_map, geo_map, training_mask)
 
-            self.optimizer.zero_grad()
-            pred_score_map, pred_geo_map, pred_recog_map = self.model(img)
+                # import cv2
+                # for i in range(img.shape[0]):
+                #     image = img[i]
+                #     for tt, bb in zip(transcripts[i], boxes[i]):
+                #         show_box(image.permute(1, 2, 0).detach().cpu().numpy()[:,:, ::-1].astype(np.uint8).copy(), bb, tt)
 
-            loss = self.loss(score_map, pred_score_map, geo_map, pred_geo_map, pred_recog_map, recog_map, training_mask)
-            loss.backward()
-            self.optimizer.step()
+                self.optimizer.zero_grad()
+                pred_score_map, pred_geo_map, pred_recog, pred_boxes, pred_mapping, indices = self.model.forward(img,
+                                                                                                                 boxes,
+                                                                                                                 mapping)
+                indice_transcripts = transcripts[indices]
+                pred_boxes = pred_boxes[indices]
+                pred_mapping = pred_mapping[indices]
+                labels, label_lengths = self.labelConverter.encode(indice_transcripts.tolist())
+                recog = (labels, label_lengths)
 
-            total_loss += loss.item()
-            #total_metrics += self._eval_metrics(output, target)
+                det_loss, reg_loss = self.loss(score_map, pred_score_map, geo_map, pred_geo_map, recog, pred_recog,
+                                               training_mask)
+                loss = det_loss + reg_loss
+                loss.backward()
+                self.optimizer.step()
 
-            total_metrics += 0
+                total_loss += loss.item()
+                pred_transcripts = []
+                if len(pred_mapping) > 0:
+                    pred_fns = [imagePaths[i] for i in pred_mapping]
 
-            if self.verbosity >= 2 and batch_idx % self.log_step == 0:
-                self.logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
-                    epoch,
-                    batch_idx * self.data_loader.batch_size,
-                    len(self.data_loader) * self.data_loader.batch_size,
-                    100.0 * batch_idx / len(self.data_loader),
-                    loss.item()))
-        # self.visdom.plot('train_loss', total_loss / len(self.data_loader))
+                    pred, lengths = pred_recog
+                    _, pred = pred.max(2)
+                    for i in range(lengths.numel()):
+                        cur_text_len = lengths[i]
+                        cur_text_pred = pred[:cur_text_len, i]
+                        t = self.labelConverter.decode(cur_text_pred, cur_text_len)
+                        pred_transcripts.append(t)
+                    pred_transcripts = np.array(pred_transcripts)
+
+                gt_fns = [imagePaths[i] for i in mapping]
+                total_metrics += self._eval_metrics((pred_boxes, pred_transcripts, pred_fns),
+                                                    (boxes, transcripts, gt_fns))
+
+                if self.verbosity >= 2 and batch_idx % self.log_step == 0:
+                    self.logger.info(
+                        'Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f} Detection Loss: {:.6f} Recognition Loss:{:.6f}'.format(
+                            epoch,
+                            batch_idx * self.data_loader.batch_size,
+                            len(self.data_loader) * self.data_loader.batch_size,
+                            100.0 * batch_idx / len(self.data_loader),
+                            loss.item(), det_loss.item(), reg_loss.item()))
+            except:
+                print(imagePaths)
+                raise
+
         log = {
             'loss': total_loss / len(self.data_loader),
-            'metrics': (total_metrics / len(self.data_loader)).tolist()
+            'precious': total_metrics[0] / len(self.data_loader),
+            'recall': total_metrics[1] / len(self.data_loader),
+            'hmean': total_metrics[2] / len(self.data_loader)
         }
 
         if self.valid:
@@ -104,26 +139,40 @@ class Trainer(BaseTrainer):
             The validation metrics in log must have the key 'val_metrics'.
         """
         self.model.eval()
-        total_val_loss = 0
-        total_val_metrics = np.zeros(len(self.metrics))
+        total_val_metrics = np.zeros(3)
         with torch.no_grad():
             for batch_idx, gt in enumerate(self.valid_data_loader):
-                img, score_map, geo_map, training_mask, transcript = gt
-                img, score_map, geo_map, training_mask = self._to_tensor(img, score_map, geo_map, training_mask)
-                recog_map = None
+                try:
+                    imagePaths, img, score_map, geo_map, training_mask, transcripts, boxes, mapping = gt
+                    img, score_map, geo_map, training_mask = self._to_tensor(img, score_map, geo_map, training_mask)
 
-                pred_score_map, pred_geo_map, pred_recog_map = self.model(img)
+                    pred_score_map, pred_geo_map, pred_recog, pred_boxes, pred_mapping, indices = self.model.forward(
+                        img, boxes, mapping)
+                    pred_transcripts = []
+                    pred_fns = []
+                    if len(pred_mapping) > 0:
+                        pred_mapping = pred_mapping[indices]
+                        pred_boxes = pred_boxes[indices]
+                        pred_fns = [imagePaths[i] for i in pred_mapping]
 
-                loss = self.loss(score_map, pred_score_map, geo_map, pred_geo_map, pred_recog_map, recog_map,
-                                 training_mask)
+                        pred, lengths = pred_recog
+                        _, pred = pred.max(2)
+                        for i in range(lengths.numel()):
+                            l = lengths[i]
+                            p = pred[:l, i]
+                            t = self.labelConverter.decode(p, l)
+                            pred_transcripts.append(t)
+                        pred_transcripts = np.array(pred_transcripts)
 
-                total_val_loss += loss.item()
+                    gt_fns = [imagePaths[i] for i in mapping]
+                    total_val_metrics += self._eval_metrics((pred_boxes, pred_transcripts, pred_fns),
+                                                            (boxes, transcripts, gt_fns))
+                except:
+                    print(imagePaths)
+                    raise
 
-                output = (pred_score_map, pred_geo_map, pred_recog_map)
-                target = (score_map, geo_map, recog_map)
-                #total_val_metrics += self._eval_metrics(output, target, training_mask) #TODO: should add AP metric
-        # self.visdom.plot('val_loss', total_val_loss / len(self.valid_data_loader))
         return {
-            'val_loss': total_val_loss / len(self.valid_data_loader),
-            'val_metrics': (total_val_metrics / len(self.valid_data_loader)).tolist()
+            'val_precious': total_val_metrics[0] / len(self.valid_data_loader),
+            'val_recall': total_val_metrics[1] / len(self.valid_data_loader),
+            'val_hmean': total_val_metrics[2] / len(self.valid_data_loader)
         }
