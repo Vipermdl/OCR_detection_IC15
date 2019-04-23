d7f18639afa7da089e4b4b88f6f3926d5425d923
luotao <luotao@renrenche.com>
2019-04-23 15:49:37 +0800

    添加常用的字符串

---
+++ utils/common_str.py
@@ -0,0 +1,4 @@
+number = '0123456789'
+alphabet = 'qwertyuiopasdfghjklzxcvbnm'+'QWERTYUIOPASDFGHJKLZXCVBNM'
+alphabet_and_number = alphabet+number
+custom_1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.'
